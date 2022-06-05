41a9c7c81856d817b7ed17d23c506aee3a387882
ahmetaric <ahmet.aric1965@gmail.com>
2022-06-05 10:36:45 +0300

    minus commit

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const plus = document.querySelector('.plus');
+const minus = document.querySelector('.minus');
 const input = document.querySelector('.input');
 const todoSection = document.querySelector("#todo-section");
 
@@ -9,7 +10,27 @@ plus.addEventListener("click",() =>{
     if(!input.value){
         alert("Boş bırakmayınız!")
     }else{
-        ul.innerHTML += `<li style="list-style-type:none">${input.value}</li>`;
+        ul.innerHTML += `<li style="list-style-type:none;">${input.value}</li>`;
         input.value = "";
     }
+});
+minus.addEventListener("click",() =>{
+    ul.childElementCount > 0
+    ? ul.removeChild(ul.lastElementChild)
+    : alert("Silinecek bir şey yok!");
 })
+
+input.addEventListener("keydown",(e) =>{
+    if(e.keyCode === 13){
+       plus.click();
+    }
+    if(e.code === "Delete"){
+        minus.click();
+    }   
+    });
+
+ 
+
+window.onload = () => {
+    input.focus();
+}
