46564ad91fc2ec085e742c1d1e25e1f4db62988d
ahmetaric <ahmet.aric1965@gmail.com>
2022-06-07 09:53:42 +0300

    remove all commit

--- app.js
+++ app.js
@@ -8,17 +8,53 @@ todoSection.appendChild(ul);
 
 plus.addEventListener("click",() =>{
     if(!input.value){
-        alert("Boş bırakmayınız!")
+        alert("This place cannot be left blank!");
     }else{
-        ul.innerHTML += `<li style="list-style-type:none;">${input.value}</li>`;
+        ul.innerHTML += `<div class="task">
+         <button class="check">
+        <i class="fa-solid fa-check"></i>
+         </button>
+          <li class="list">${input.value}</li>
+          <button class="delete">
+            <i class="far fa-trash-alt"></i>
+         </button>
+         </div>`;
         input.value = "";
+
+        let close = document.querySelectorAll(".delete");
+        for(let i = 0; i < close.length; i++){
+            close[i].onclick = function(){
+                this.parentNode.remove();
+            }
+        }
+
+        let check = document.querySelectorAll(".check");
+        for(let i = 0; i < check.length; i++){
+            check[i].onclick = function(){
+                this.parentNode.style.textDecoration = "line-through";
+                this.parentNode.style.color = "aqua";
+                this.nextElementSibling.style.color = "aqua";
+                this.style.backgroundColor = "green";
+            }
+        }
+        for (let i = 0; i < check.length; i++) {
+          check[i].ondblclick = function () {
+            this.parentNode.style.textDecoration = "none";
+            this.nextElementSibling.style.opacity = "1";
+            this.nextElementSibling.style.color = "black";
+            this.style.backgroundColor = "white";
+            this.nextElementSibling.style.fontStyle = "normal";
+          };
+        }
     }
 });
-minus.addEventListener("click",() =>{
-    ul.childElementCount > 0
-    ? ul.removeChild(ul.lastElementChild)
-    : alert("Silinecek bir şey yok!");
-})
+// minus.addEventListener("click",() =>{
+//     ul.childElementCount > 0
+//     ? ul.removeChild(ul.lastElementChild)
+//     : alert("There is nothing to delete!");
+// })
+
+
 
 input.addEventListener("keydown",(e) =>{
     if(e.keyCode === 13){
@@ -29,8 +65,12 @@ input.addEventListener("keydown",(e) =>{
     }   
     });
 
- 
+ function removeAll() {
+   document.querySelector("ul").innerHTML = "";
+ }
 
-window.onload = () => {
-    input.focus();
-}
+// window.onload = () => {
+//     input.focus();
+// }
+
+input.focus();
